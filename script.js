f5b4c15b51fa5d84b59e39259a9d57753376e85e
WebWithRathor <devrajrathor143@gmail.com>
2023-09-20 10:50:27 -1200

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+var rn = ``
+function bubly() {
+  var clutter = ``;
+  for (var i = 0; i <= 150; i++) {
+    var valu = Math.floor(Math.random() * 10);
+    clutter += `<div class="bubble">${valu}</div>`;
+  }
+  document.querySelector("#bottom").innerHTML = clutter
+}
+function timer() {
+  var time = 60;
+    var timers = setInterval(function () {
+  if (time > 0) {
+      time--;
+      document.querySelector(".box #time").textContent = time;
+  } else {
+    clearInterval(timers);
+    gsap.timeline()
+    .to(".bubble",{
+      scale:0,
+      opacity:0
+    })
+    .to("#gameover",{
+      scale:1
+    })
+    document.querySelector("#gameover h2>span").textContent=score
+
+  }
+    }, 1000)
+}
+function hit() {
+  rn = Math.floor(Math.random() * 10);
+  document.querySelector(".box #hit").textContent = rn;
+}
+var score = 0;
+function add(){
+  score += 10;
+document.querySelector(".box #score").textContent = score;
+}
+function scoreadd() {
+  document.querySelector("#bottom").addEventListener("click", function (dets) {
+    var spot = Number(dets.target.textContent);
+    if (spot === rn) {
+      add();
+      bubly();
+      hit();
+    }
+  })
+}
+scoreadd();
+hit();
+setTimeout(function(){
+  bubly();
+timer();
+},500)
+
+
+document.querySelector("#gameover button").addEventListener("click",function(){
+  location.reload();
+})
